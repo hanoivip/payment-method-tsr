a8a0c00298ccbc25edd93866f5f6017ef0bb9c7e
wartune <wartune.mobi@gmail.com>
2021-08-08 13:57:20 +0700

    f

--- src/HelperTestDelay.php
+++ src/HelperTestDelay.php
@@ -2,6 +2,8 @@
 
 namespace Hanoivip\PaymentMethodTsr;
 
+use Illuminate\Support\Facades\Cache;
+
 
 class HelperTestDelay implements IHelper
 {
@@ -17,10 +19,24 @@ class HelperTestDelay implements IHelper
     public function check($partnerId, $partnerSecret,
         $serial, $password, $telco, $dvalue, $mapping)
     {
-        $test = array(
-            'status' => 1, 'message' => 'The dung', 'value' => 100000,
-            'amount' => 0, 'code' => $password, 'serial' => $serial,
-            'request_id' => mt_rand(10000, 50000), 'telco' => $telco);
+        $delay = 0;
+        if (Cache::has('TsrHelperTestDelay'))
+        {
+            $delay = Cache::get('TsrHelperTestDelay');
+        }
+        if ($delay < 3)
+        {
+            $test = array(
+                'status' => 99, 'message' => 'The tre');
+            return json_encode($test);
+        }
+        else 
+            {
+            $test = array(
+                'status' => 1, 'message' => 'The dung', 'value' => 100000,
+                'amount' => 0, 'code' => $password, 'serial' => $serial,
+                'request_id' => mt_rand(10000, 50000), 'telco' => $telco);
+        }
         return json_encode($test);
     }
     
