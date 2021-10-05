d5b0a1242ffd696cb1d63ebb3a938dec56976a7c
hanoivip <game.oh.vn@gmail.com>
2021-10-05 11:23:25 +0700

    f

---
+++ lang/en/tsr.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'messages' => [
+        99 => 'Still in process.. plz kindly wait..',
+        1 => 'Valid card',
+        2 => 'Valid card',
+        3 => 'Invalid card',
+        4 => 'Payment is in maintanance. Plz try later.',
+        324 => 'Payment is in maintanance. Plz try later.',
+        309 => 'Invalid card',
+        311 => 'Invalid card',
+        307 => 'Submitted card',
+        327 => 'Payment is in maintanance. Plz try later.',
+        317 => 'Payment is in maintanance. Plz try later.',
+        329 => 'Payment is in maintanance. Plz try later.',
+        100 => 'Card password is wrong. Try again.'
+    ],
+    'guide' => 'Payment by telecom and/or game cards. More details: https://bombman.vn1.us/blog/card-payment-guide',
+    'failure' => [
+        'missing-params' => 'You have to input all field (card serial, card password, card value)',
+        'invalid-trans' => 'Payment failure. Plz try again (1)',
+        'exception' => 'Payment error. Plz try again',
+        'invalid-trans2' => 'Payment failure. Plz try again (2)',
+    ],
+];
