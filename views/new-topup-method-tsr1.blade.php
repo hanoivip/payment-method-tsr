929b2a7104ca99c9f4e689491f0f4899452ff22b
unknown <game.oh.vn@gmail.com>
2023-03-30 08:49:04 +0700

    f

--- views/new-topup-method-tsr1.blade.php
+++ views/new-topup-method-tsr1.blade.php
@@ -11,14 +11,14 @@
 <form method="post" action="{{route('newtopup.do')}}">
 {{ csrf_field() }}
 	<input type="hidden" id="trans" name="trans" value="{{$trans}}"/>
-	Loại thẻ: <select id="cardtype" name="cardtype">
+	Card type: <select id="cardtype" name="cardtype">
 		@foreach (Config::get('payment.tsr.telco_static') as $cardtype => $title))
 			<option value="{{$cardtype}}">{{$title}}</option>
 		@endforeach 
 	</select>
-	Seri thẻ: <input id="cardseri" name="cardseri" value=""/>
-	Mã thẻ: <input id="cardpass" name="cardpass" value=""/>
-	Giá trị thẻ: <select id="dvalue" name="dvalue">
+	Card serial: <input id="cardseri" name="cardseri" value=""/>
+	Card password: <input id="cardpass" name="cardpass" value=""/>
+	Card amount: <select id="dvalue" name="dvalue">
 		@foreach (Config::get('payment.tsr.values_static') as $value => $title)
 			<option value="{{$value}}">{{$title}}</option>
 		@endforeach
