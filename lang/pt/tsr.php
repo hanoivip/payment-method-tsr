4efff983ed49c4de0bb7f90801289bf6d48176bc
unknown <game.oh.vn@gmail.com>
2024-10-01 12:50:02 +0700

    f

---
+++ lang/pt/tsr.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'messages' => [
+        99 => 'Ainda em processo.. por favor, aguarde..',
+        1 => 'Cartão válido',
+        2 => 'Cartão válido',
+        3 => 'Cartão inválido',
+        4 => 'O pagamento está em manutenção. Por favor, tente mais tarde.',
+        324 => 'O pagamento está em manutenção. Por favor, tente mais tarde.',
+        309 => 'Cartão inválido',
+        311 => 'Cartão inválido',
+        307 => 'Cartão enviado',
+        327 => 'O pagamento está em manutenção. Por favor, tente mais tarde.',
+        317 => 'O pagamento está em manutenção. Por favor, tente mais tarde.',
+        329 => 'O pagamento está em manutenção. Por favor, tente mais tarde.',
+        100 => 'A senha do cartão está errada. Tente novamente.',
+        101 => 'Cartão não existe. Entre em contato com o vendedor para verificar este cartão.',
+        102 => 'Erro de sistema. Entre em contato com o suporte ao cliente! (solicitado dup)',
+        //definido pelo usuário
+        999 => 'Tsr é mantido. Por favor, tente novamente mais tarde.',
+    ],
+    'guide' => 'Pagamento por telecomunicações e/ou cartões de jogo. Mais detalhes: https://bombheroesbr.com/blog/diretrizes-para-pagar-com-carto-zing',
+    'failure' => [
+        'missing-params' => 'Você deve inserir todos os campos (série do cartão, senha do cartão, valor do cartão)',
+        'invalid-trans' => 'Falha no pagamento. Por favor, tente novamente (1)',
+        'exception' => 'Erro no pagamento. Por favor, tente novamente',
+        'invalid-trans2' => 'Falha no pagamento. Por favor, tente novamente (2)',
+    ],
+];
